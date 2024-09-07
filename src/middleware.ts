d4d852d7fd86362967d6ee73f5397c9aa50a7f24
Hoang Van Nguyen <hoang.nguyenvan@asnet.com.vn>
2024-09-07 20:55:26 +0700

    Update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,20 +7,20 @@ import { authConfig } from "./auth.config";
 export default NextAuth(authConfig).auth;
 
 // Handle request HTTP before they come to page or API route
-export async function middleware(req: NextRequest) {
-  // Get token form cookie or header
-  const token = await getToken({
-    req,
-    secret: process.env.AUTH_SECRET || "",
-  } as unknown as GetTokenParams);
+// export async function middleware(req: NextRequest) {
+//   // Get token form cookie or header
+//   const token = await getToken({
+//     req,
+//     secret: process.env.AUTH_SECRET || "",
+//   } as unknown as GetTokenParams);
 
-  if (!token) {
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
+//   if (!token) {
+//     return NextResponse.redirect(new URL("/login", req.url));
+//   }
 
-  // Continue if token is valid
-  return NextResponse.next();
-}
+//   // Continue if token is valid
+//   return NextResponse.next();
+// }
 
 export const config = {
   matcher: [
